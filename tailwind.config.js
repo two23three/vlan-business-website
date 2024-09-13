cd3edca8c81d80ec7a924fda9dedfc6182430ccc
Tulley Mwenda <Tulleyteyez@gmail.com>
2024-09-13 13:26:09 +0300

    done fixing errors

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,6 +72,16 @@ module.exports = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+    animation: {
+      "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+    },
+    keyframes: {
+      "border-beam": {
+        "100%": {
+          "offset-distance": "100%",
+        },
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 }
